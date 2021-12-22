3b4d32761bc490510b26f41b5a0248b08fe9928c
Mike Burr <mburr@unintuitive.org>
2021-12-21 18:13:26 -0700

    I think these go here.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='finite_fields',
+      version='0.0.1',
+      description='https://github.com/j2kun/finite-fields',
+      author='https://github.com/j2kun',
+      author_email='j2kun@j2.kun',
+      url='https://github.com/j2kun/finite-fields',
+      #packages=['finite_fields'],
+     )
